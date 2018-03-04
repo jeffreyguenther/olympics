fa53b9500a5e83eaeccbfd66db36334a73804e4d
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2018-03-03 23:05:26 -0800

    Adds LiftResult
    LiftResults determine whether a lift is successful given
    a particular attempt.
    
    If no result is provided, a random result is generated

---
+++ app/models/lift_result.rb
@@ -0,0 +1,39 @@
+class LiftResult
+  attr_reader :attempt, :result
+
+  def initialize(attempt:, result: nil)
+    @attempt = attempt
+    @result = result || generate_random_result
+  end
+
+  def success?
+    case attempt
+    when 0
+      make_first_attempt
+    when 1
+      make_second_attempt
+    when 2
+      make_third_attempt
+    else
+      false
+    end
+  end
+
+  private
+    def generate_random_result
+      prng = Random.new
+      prng.rand(0..3)
+    end
+
+    def make_first_attempt
+      (0..2).include?(@result)
+    end
+
+    def make_second_attempt
+      (0..1).include?(@result)
+    end
+
+    def make_third_attempt
+      0 == @result
+    end
+end
