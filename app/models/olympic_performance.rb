1efb6c559ff024982f31a6e04a2ff0f00e1ccadf
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2018-03-05 12:22:07 -0800

    Adds OlympicPerformance
    This object is responsible for generating the three attempts
    allowed by an athlete during an olympic lifing event

---
+++ app/models/olympic_performance.rb
@@ -0,0 +1,18 @@
+class OlympicPerformance
+  attr_reader :results
+
+  def initialize(starting_weight:)
+    @starting_weight = starting_weight
+  end
+
+  def generate
+    @results = []
+    current_weight = @starting_weight
+
+    3.times do |attempt|
+      attempt = LiftAttempt.new(attempt: attempt, weight: current_weight)
+      current_weight = attempt.next_weight
+      @results.push(attempt)
+    end
+  end
+end
---
+++ test/models/olympic_performance_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class OlympicPerformanceTest < ActiveSupport::TestCase
+  test "#results after three attempts" do
+    performance = OlympicPerformance.new(starting_weight: 100)
+    performance.generate
+
+    assert performance.results.count, 3
+
+    first, second, _ = performance.results
+    if first.success?
+      assert second.weight > first.weight
+    else
+      assert_equals second.weight, first.weight
+    end
+  end
+end
