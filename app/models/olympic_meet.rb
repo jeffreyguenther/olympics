433c0ee5d4f761924dcd731e6dc4e94ea225721a
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2018-03-05 11:18:32 -0800

    Adds methods to create starting weights for olympic lifting meets

--- app/models/olympic_meet.rb
+++ app/models/olympic_meet.rb
@@ -2,4 +2,27 @@ class OlympicMeet
   def self.movements
     ["snatch", "clean & jerk"]
   end
+
+  def self.starting_snatch_weight
+    generate_rounded_weight_between((100..185))
+  end
+
+  def self.starting_clean_and_jerk_weight
+    generate_rounded_weight_between(135..185)
+  end
+
+  private
+    def self.generate_weight_in(range)
+      @@prng ||= Random.new
+      weight = @@prng.rand(range)
+    end
+
+    def self.generate_rounded_weight_between(range)
+      weight = generate_weight_in(range)
+      round_to_nearest_five_pounds(weight)
+    end
+
+    def self.round_to_nearest_five_pounds(weight)
+      (weight / 5.0).round * 5
+    end
 end
--- test/models/olympic_meet_test.rb
+++ test/models/olympic_meet_test.rb
@@ -6,4 +6,22 @@ class OlympicMeetTest < ActiveSupport::TestCase
 
     assert_equal OlympicMeet.movements, expected_movements
   end
+
+  test "generate starting snatch weight" do
+    expected_range = (100..185)
+
+    weight = OlympicMeet.starting_snatch_weight
+
+    assert_includes expected_range, weight
+    assert_equal weight % 5, 0
+  end
+
+  test "generate starting clean and jerk weight" do
+    expected_range = (135..185)
+
+    weight = OlympicMeet.starting_clean_and_jerk_weight
+
+    assert_includes expected_range, weight
+    assert_equal weight % 5, 0
+  end
 end
