a414d2db8878c6b5629b70e0e19a94318f4dde88
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2018-03-12 13:44:04 -0700

    Completes row result test

--- app/models/random/run_result.rb
+++ app/models/random/run_result.rb
@@ -1,16 +1,4 @@
 class Random::RunResult < Random::DistanceResult
-  attr_reader :speed, :distance
-
-  def initialize(distance:, speed: nil, result: nil)
-    @distance = distance
-    @speed = speed || generate_speed
-    @result = result || generate_time
-  end
-
-  def result
-    @result.to_i
-  end
-
   protected
     def generate_500m_time(speed)
       case speed
@@ -43,41 +31,5 @@ class Random::RunResult < Random::DistanceResult
       when :fast
         generate_run_time_between(13.minute + 30.seconds..15.minutes)
       end
-    end
-
-  private
-    def generate_time
-      case distance
-      when 500
-        generate_500m_time(speed)
-      when 1000
-        generate_1000m_time(speed)
-      when 5000
-        generate_5000m_time(speed)
-      end
-    end
-
-    def generate_run_time_between(range)
-      random_between(range)
-    end
-
-    def generate_speed
-      convert_speed_to_symbol(random_between(0..2))
-    end
-
-    def convert_speed_to_symbol(speed)
-      case speed
-      when 0
-        :slow
-      when 1
-        :medium
-      when 2
-        :fast
-      end
-    end
-
-    def random_between(range)
-      @@prng ||= Random.new
-      @@prng.rand(range)
     end
 end
