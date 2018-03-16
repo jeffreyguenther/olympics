1e13392ce0fbccd15d1b7e42902c7c5e1c799d3a
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2018-03-16 15:09:54 -0700

    Updates in memory copy import to import winners

--- app/models/import/copy_from_in_memory.rb
+++ app/models/import/copy_from_in_memory.rb
@@ -17,7 +17,8 @@ class Import::CopyFromInMemory
 
         event = build_event(event_data.type)
 
-        stream_athletes(event, event_data.performances)
+        stream_attempts(event, event_data.performances)
+        stream_winners(event, event_data.winners)
       end
     end
   end
@@ -31,16 +32,27 @@ class Import::CopyFromInMemory
       Event.create(kinds: type)
     end
 
-    def stream_athletes(event, performances)
-      performances.each { |p| p.results.each { |r| stream_data_via_copy(event, p.athlete, p.movement, r)}}
+    def stream_attempts(event, performances)
+      performances.each { |p| p.results.each { |r| stream_attempt_data_via_copy(event, p.athlete, p.movement, r)}}
     end
 
-    def stream_data_via_copy(event, athlete, movement, attempt)
-      @pg_connection.copy_data("COPY attempts (result, attempt, athlete_id, movement_id, created_at, updated_at, event_id, success) FROM STDIN", @pg_encoding) do
-        @pg_connection.put_copy_data([
-          attempt.score, attempt.attempt, athlete.id, @movement_ids[movement],
+    def stream_winners(event, winners)
+      columns = ["event_id", "athlete_id", "created_at", "updated_at"]
+      winners.each do |w|
+        stream_data_via_copy("winners", columns, [event.id, w.id, Time.now.to_s, Time.now.to_s])
+      end
+    end
+
+    def stream_attempt_data_via_copy(event, athlete, movement, attempt)
+      columns = ["result", "attempt", "athlete_id", "movement_id", "created_at", "updated_at", "event_id", "success"]
+      data = [attempt.score, attempt.attempt, athlete.id, @movement_ids[movement],
           Time.now.to_s, Time.now.to_s, event.id, attempt.success?
-        ])
+        ]
+    end
+
+    def stream_data_via_copy(table, columns, data)
+      @pg_connection.copy_data("COPY #{table} (#{columns.join(",")}) FROM STDIN", @pg_encoding) do
+        @pg_connection.put_copy_data(data)
       end
     end
 end
